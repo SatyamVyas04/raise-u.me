88818d9ad7ddcf56b3780487e97445ed52cbb280
Tanush Salian <tanush1852@gmail.com>
2024-09-09 14:45:06 +0530

    enhancer and dashboard

--- client/src/app/(root)/enhancer/page.tsx
+++ client/src/app/(root)/enhancer/page.tsx
@@ -1,7 +1,36 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import AddYourResume from "@/components/addYourResume";
+import img3 from '@/components/images/image.png'
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 export default function page() {
 	return (
-		<div>
-			<h1>Resume Enhancer</h1>
+		<div className="w-full h-full flex items-center justify-center align-middle flex-col gap-10">
+			
+			<p className="text-5xl font-semibold hover:text-slate-400 hover:cursor-pointer mt-40">Resume Enhancer</p>
+            <div className="w-[600px] h-[300px] bg-gray-600 rounded-2xl">
+				<div className="bg-slate-800 h-10 items-center flex justify-center text-xl rounded-md">
+					<p className=" text-white rounded-xl px-1 font-semibold">Upload your Resume</p>
+				</div>
+				<div className="m-10 flex justify-center items-center">
+                <Image
+                src={img3}
+                alt="Description of the image"
+                layout="fit"
+                objectFit="cover"
+				className="h-48 transition-transform duration-300 ease-in-out hover:scale-105 hover:cursor-pointer"
+                />
+				</div>
+				<div className="flex items-center justify-center mt-20">
+				<Button className="bg-slate-200 w-52 h-12 rounded-md hover:bg-gray-200 ">
+					<p className="text-black p-2 text-lg font-semibold">Enhance your Resume</p>
+				</Button>
+				</div>
+			</div>
+
+		
+			
+			
 		</div>
 	);
 }
--- client/src/app/(root)/savedtemplates/page.tsx
+++ client/src/app/(root)/savedtemplates/page.tsx
@@ -1,15 +1,21 @@
-import img1 from '@/components/images/image.png'; // Importing the image from the specified path
+import img1 from '@/components/images/image1.png'; // Importing the image from the specified path
 import Image from 'next/image';
+import ImageCard from '@/components/imageCard';
+import img2 from '@/components/images/image2.png'
+
 export default function Page() {
-	return (
-		<div>
-			<p className="text-white text-5xl font-semibold mt-10">Saved Templates</p>
-            <div className='mt-8 flex gap-28 '>
-                <Image src={img1} alt="Description of the image" className="w-[215px] h-[297px] hover:cursor-pointer hover:scale-200"  />
-                <Image src={img1} alt="Description of the image" className="w-[215px] h-[297px] hover:cursor-pointer hover:scale-200"  />
-                <Image src={img1} alt="Description of the image" className="w-[215px] h-[297px] hover:cursor-pointer hover:scale-200"  />
-                <Image src={img1} alt="Description of the image" className="w-[215px] h-[297px] hover:cursor-pointer hover:scale-200"  /> {/* Using the imported image */}
+    return (
+        <div>
+            <p className="text-white text-5xl font-semibold mt-10 hover:text-purple-500 hover:cursor-pointer stagger-text ">Saved Templates</p>
+            <div className='mt-8 flex flex-wrap gap-8'>
+
+                <ImageCard image1={img1} />
+                <ImageCard image1={img1} />
+                <ImageCard image1={img1} />
+                <ImageCard image1={img1} />
+                <ImageCard image1={img2}/>
+                
             </div>
-		</div>
-	);
+        </div>
+    );
 }
