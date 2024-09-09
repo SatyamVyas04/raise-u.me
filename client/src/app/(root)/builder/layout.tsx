6186094f7495c9bb12e2172b4ce56779809f0398
Tanush Salian <tanush1852@gmail.com>
2024-09-09 15:20:24 +0530

    fixed

--- client/src/app/(root)/builder/layout.tsx
+++ client/src/app/(root)/builder/layout.tsx
@@ -1,1 +1,19 @@
+import React, { ReactNode } from "react";
+import Navbar from "@/components/layout/Navbar";
+import Sidebar from "@/components/layout/Sidebar";
+const HomeLayout = ({ children }: { children: ReactNode }) => {
+	return (
+		<main className="relative">
+			<Navbar />
+			<div className="flex">
+                <Sidebar/>
+				<section className="flex min-h-screen flex-1 flex-col">
+					<div className="w-full">{children}</div>
+				</section>
+			</div>
+		</main>
+	);
+};
+
+export default HomeLayout;
 
