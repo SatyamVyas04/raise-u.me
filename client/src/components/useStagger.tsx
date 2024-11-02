dbadd90cc9e77561b439ef7461a6b50382aeba2a
Satyam Vyas <vyas.satyam.04@gmail.com>
2024-11-02 23:28:55 +0530

    style: format code for consistency and readability

--- client/src/components/useStagger.tsx
+++ client/src/components/useStagger.tsx
@@ -1,18 +1,18 @@
 // useStaggerAnimation.ts
-import { useEffect } from 'react';
-import { gsap } from 'gsap';
+import { useEffect } from "react";
+import { gsap } from "gsap";
 
 const useStagger = (selector: string) => {
-  useEffect(() => {
-    const elements = document.querySelectorAll(selector);
-    gsap.from(elements, {
-      opacity: 0,
-      y: 50,
-      duration: 1,
-      stagger: 0.2,
-      ease: 'power2.out',
-    });
-  }, [selector]);
+	useEffect(() => {
+		const elements = document.querySelectorAll(selector);
+		gsap.from(elements, {
+			opacity: 0,
+			y: 50,
+			duration: 1,
+			stagger: 0.2,
+			ease: "power2.out",
+		});
+	}, [selector]);
 };
 
 export default useStagger;
